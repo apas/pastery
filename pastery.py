767fc5e7b59fd807b084070396d5dbc589dab5d6
apas <apostolos.me@gmail.com>
2015-12-08 21:09:10 +0100

    paste them all

---
+++ pastery.py
@@ -0,0 +1,25 @@
+import sublime, sublime_plugin, json, subprocess
+from subprocess import Popen, PIPE, STDOUT
+
+class PasteryCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    content = u''
+    for region in self.view.sel():
+      if not region.empty():
+          content += self.view.substr(region)
+
+    url = "https://www.pastery.net/api/paste/?"
+    req = Request(url,
+      data=bytes(content),
+      headers={'User-Agent': 'Mozilla/5.0 Sublime Text Pastery plugin'})
+    curl = "curl -X POST "+url
+    response = subprocess.Popen(["curl", "-X", "POST", url, "--data", content], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = response.communicate()
+    rurl = json.loads(stdout)["url"]
+    print rurl
+
+    if rurl:
+      sublime.set_clipboard(str(rurl))
+      sublime.status_message("Paste: " + str(rurl))
+    else:
+      print("Error while pasting.")
---
+++ readme.md
@@ -0,0 +1,17 @@
+# pastery
+
+**pastery** is a Sublime Text plugin for [pastery][p] - *the sweetest pastebin in the world.*
+
+## Installation
+
+Through Package Manager.
+
+## Usage
+
+Select any text and `cmd` + `option` + `c` and the pastery link will be ready to be pasted anywhere from your clipboard.
+
+## License
+
+MIT
+
+[p]: http://pastery.net
